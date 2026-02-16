d18651a687b852d46c66a46f04c4cd9dc7e97783
TwsitedWizard <saranshabu@gmail.com>
2026-02-16 10:36:46 +0530

    First commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package TestibgStuffs;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Another Guy Trying to not get Sleep.");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
